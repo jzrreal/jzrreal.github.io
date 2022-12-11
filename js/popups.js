f5492ab7ab2660796cb4cc339e9ce30e2f3ea462
jzrreal <jezriellukas@gmail.com>
2022-12-11 17:35:50 +0700

    Update popups.js
    user.updateProfile({ displayName: username })
    changed to:
    updateProfile(user, { displayName: username })

--- js/popups.js
+++ js/popups.js
@@ -37,7 +37,7 @@ function newUserLogin() {
     let username = document.getElementById('username-input').value
     
     let user = auth.currentUser;
-    user.updateProfile({ displayName: username })
+    updateProfile(user, { displayName: username })
 
     db.collection("users").doc(user.uid).set({ name: username, admin: false })
     loginModal.hide()
