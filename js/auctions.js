2fc1b023a112386ad3ac42ea9b23d5688f0ccc01
jezriellukas <jezriel@student.ub.ac.id>
2022-12-14 18:59:23 +0700

    Update auctions.js

--- js/auctions.js
+++ js/auctions.js
@@ -32,9 +32,9 @@ let startingPrices = [
   15 // 3
 ];
 let endTimes = [
-  1671019200000, // 1 
-  1671019200000, // 2
-  1671019200000 // 3
+  1671026400000, // 1 
+  1671026400000, // 2
+  1671026400000 // 3
 ]; // Make sure to fix these to UTC time so they don't change with the users timezone
 
 // Random auction information
